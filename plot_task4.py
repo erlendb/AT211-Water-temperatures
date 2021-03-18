93387bb20d297cc2897e8f773badf43efc028895
Erlend Blomseth <erlend@elendig.no>
2021-03-18 14:10:47 +0100

    add polynomial approx in task 4

--- plot_task4.py
+++ plot_task4.py
@@ -1,4 +1,5 @@
 from process_data import *
+import numpy as np
 
 # Find the breakpoints
 break_temperatures = [0, 1, -1]
@@ -30,6 +31,27 @@ p9, = plt.plot(hours[0:breakpoints[0][9]], stringTemperatures[9][0:breakpoints[0
 p10, = plt.plot(hours[0:breakpoints[0][10]], stringTemperatures[10][0:breakpoints[0][10]])
 p11, = plt.plot(hours[0:breakpoints[0][11]], stringTemperatures[11][0:breakpoints[0][11]])
 
+p5_trend = np.poly1d(np.polyfit(hours[0:breakpoints[0][5]],stringTemperatures[5][0:breakpoints[0][5]], 2))
+plt.plot(hours[0:breakpoints[0][5]], p5_trend(hours[0:breakpoints[0][5]]))
+
+p6_trend = np.poly1d(np.polyfit(hours[0:breakpoints[0][6]],stringTemperatures[6][0:breakpoints[0][6]], 2))
+plt.plot(hours[0:breakpoints[0][6]], p6_trend(hours[0:breakpoints[0][6]]))
+
+p7_trend = np.poly1d(np.polyfit(hours[0:breakpoints[0][7]],stringTemperatures[7][0:breakpoints[0][7]], 2))
+plt.plot(hours[0:breakpoints[0][7]], p7_trend(hours[0:breakpoints[0][7]]))
+
+p8_trend = np.poly1d(np.polyfit(hours[0:breakpoints[0][8]],stringTemperatures[8][0:breakpoints[0][8]], 2))
+plt.plot(hours[0:breakpoints[0][8]], p8_trend(hours[0:breakpoints[0][8]]))
+
+p9_trend = np.poly1d(np.polyfit(hours[0:breakpoints[0][9]],stringTemperatures[9][0:breakpoints[0][9]], 2))
+plt.plot(hours[0:breakpoints[0][9]], p9_trend(hours[0:breakpoints[0][9]]))
+
+p10_trend = np.poly1d(np.polyfit(hours[0:breakpoints[0][10]],stringTemperatures[10][0:breakpoints[0][10]], 2))
+plt.plot(hours[0:breakpoints[0][10]], p10_trend(hours[0:breakpoints[0][10]]))
+
+p11_trend = np.poly1d(np.polyfit(hours[0:breakpoints[0][11]],stringTemperatures[11][0:breakpoints[0][11]], 2))
+plt.plot(hours[0:breakpoints[0][11]], p11_trend(hours[0:breakpoints[0][11]]))
+
 fig.suptitle(plot_title)
 
 plt.ylabel('Degrees [C]')
@@ -51,6 +73,27 @@ p8, = plt.plot(hours[breakpoints[1][8]:breakpoints[2][8]], stringTemperatures[8]
 p9, = plt.plot(hours[breakpoints[1][9]:breakpoints[2][9]], stringTemperatures[9][breakpoints[1][9]:breakpoints[2][9]])
 p10, = plt.plot(hours[breakpoints[1][10]:breakpoints[2][10]], stringTemperatures[10][breakpoints[1][10]:breakpoints[2][10]])
 p11, = plt.plot(hours[breakpoints[1][11]:breakpoints[2][11]], stringTemperatures[11][breakpoints[1][11]:breakpoints[2][11]])
+
+p5_trend = np.poly1d(np.polyfit(hours[breakpoints[1][5]:breakpoints[2][5]],stringTemperatures[5][breakpoints[1][5]:breakpoints[2][5]], 2))
+plt.plot(hours[breakpoints[1][5]:breakpoints[2][5]], p5_trend(hours[breakpoints[1][5]:breakpoints[2][5]]))
+
+p6_trend = np.poly1d(np.polyfit(hours[breakpoints[1][6]:breakpoints[2][6]],stringTemperatures[6][breakpoints[1][6]:breakpoints[2][6]], 2))
+plt.plot(hours[breakpoints[1][6]:breakpoints[2][6]], p6_trend(hours[breakpoints[1][6]:breakpoints[2][6]]))
+
+p7_trend = np.poly1d(np.polyfit(hours[breakpoints[1][7]:breakpoints[2][7]],stringTemperatures[7][breakpoints[1][7]:breakpoints[2][7]], 2))
+plt.plot(hours[breakpoints[1][7]:breakpoints[2][7]], p7_trend(hours[breakpoints[1][7]:breakpoints[2][7]]))
+
+p8_trend = np.poly1d(np.polyfit(hours[breakpoints[1][8]:breakpoints[2][8]],stringTemperatures[8][breakpoints[1][8]:breakpoints[2][8]], 2))
+plt.plot(hours[breakpoints[1][8]:breakpoints[2][8]], p8_trend(hours[breakpoints[1][8]:breakpoints[2][8]]))
+
+p9_trend = np.poly1d(np.polyfit(hours[breakpoints[1][9]:breakpoints[2][9]],stringTemperatures[9][breakpoints[1][9]:breakpoints[2][9]], 2))
+plt.plot(hours[breakpoints[1][9]:breakpoints[2][9]], p9_trend(hours[breakpoints[1][9]:breakpoints[2][9]]))
+
+p10_trend = np.poly1d(np.polyfit(hours[breakpoints[1][10]:breakpoints[2][10]],stringTemperatures[10][breakpoints[1][10]:breakpoints[2][10]], 2))
+plt.plot(hours[breakpoints[1][10]:breakpoints[2][10]], p10_trend(hours[breakpoints[1][10]:breakpoints[2][10]]))
+
+p11_trend = np.poly1d(np.polyfit(hours[breakpoints[1][11]:breakpoints[2][11]],stringTemperatures[11][breakpoints[1][11]:breakpoints[2][11]], 2))
+plt.plot(hours[breakpoints[1][11]:breakpoints[2][11]], p11_trend(hours[breakpoints[1][11]:breakpoints[2][11]]))
 
 fig.suptitle(plot_title)
 
@@ -74,6 +117,27 @@ p8, = plt.plot(hours[breakpoints[0][8]::], stringTemperatures[8][breakpoints[0][
 p9, = plt.plot(hours[breakpoints[0][9]::], stringTemperatures[9][breakpoints[0][9]::])
 p10, = plt.plot(hours[breakpoints[0][10]::], stringTemperatures[10][breakpoints[0][10]::])
 p11, = plt.plot(hours[breakpoints[0][11]::], stringTemperatures[11][breakpoints[0][11]::])
+
+p5_trend = np.poly1d(np.polyfit(hours[breakpoints[0][5]::],stringTemperatures[5][breakpoints[0][5]::], 2))
+plt.plot(hours[breakpoints[0][5]::], p5_trend(hours[breakpoints[0][5]::]))
+
+p6_trend = np.poly1d(np.polyfit(hours[breakpoints[0][6]::],stringTemperatures[6][breakpoints[0][6]::], 2))
+plt.plot(hours[breakpoints[0][6]::], p6_trend(hours[breakpoints[0][6]::]))
+
+p7_trend = np.poly1d(np.polyfit(hours[breakpoints[0][7]::],stringTemperatures[7][breakpoints[0][7]::], 2))
+plt.plot(hours[breakpoints[0][7]::], p7_trend(hours[breakpoints[0][7]::]))
+
+p8_trend = np.poly1d(np.polyfit(hours[breakpoints[0][8]::],stringTemperatures[8][breakpoints[0][8]::], 2))
+plt.plot(hours[breakpoints[0][8]::], p8_trend(hours[breakpoints[0][8]::]))
+
+p9_trend = np.poly1d(np.polyfit(hours[breakpoints[0][9]::],stringTemperatures[9][breakpoints[0][9]::], 2))
+plt.plot(hours[breakpoints[0][9]::], p9_trend(hours[breakpoints[0][9]::]))
+
+p10_trend = np.poly1d(np.polyfit(hours[breakpoints[0][10]::],stringTemperatures[10][breakpoints[0][10]::], 2))
+plt.plot(hours[breakpoints[0][10]::], p10_trend(hours[breakpoints[0][10]::]))
+
+p11_trend = np.poly1d(np.polyfit(hours[breakpoints[0][11]::],stringTemperatures[11][breakpoints[0][11]::], 2))
+plt.plot(hours[breakpoints[0][11]::], p11_trend(hours[breakpoints[0][11]::]))
 
 fig.suptitle(plot_title)
 
